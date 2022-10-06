536c5316c52ac4515752be8e9cbb47146e24670c
Hugo <Hugo@HUGOPC>
2022-10-06 20:44:44 +0200

    fini

---
+++ main.py
@@ -0,0 +1,11 @@
+from fileinput import filename
+from pytube import YouTube
+
+link = input("Met le lien de la video:")
+yt = YouTube(link)
+
+downloader = yt.streams.get_highest_resolution()
+print("en telechargement")
+
+downloader.download(filename="YTBvideo.mp4")
+print("fini")s
