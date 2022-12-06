c0fef918a91ad6cea9636ea72ca09a9eba6381b2
Hugo <hugopc@dualboot.hp>
2022-12-06 18:55:21 +0100

    *

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from fileinput import filename
 from pytube import YouTube
 
 link = input("Met le lien de la video:\n")
+format = input("En quoi le veux tu: (1 - mp3) (2 - mp4)")
 yt = YouTube(link)
+
+def mp3():
+    downloader.download(filename="YTBvideo.mp3")
+def mp4():
+    downloader.download(filename="YTBvideo.mp4")
 
 downloader = yt.streams.get_highest_resolution()
 print("en telechargement")
 
-downloader.download(filename="YTBvideo.mp4")
+if format == "1":
+    mp3()
+else:
+    mp4()
+
 print("fini")
